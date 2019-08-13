c2fa71d202fa2a9e01d0afebaa64a7afc0a80feb
zaboevai <zaboevai@yandex.ru>
2019-08-13 23:32:48 +0300

    extract output to func "print_calc_result"

--- analyze_projects.py
+++ analyze_projects.py
@@ -1,6 +1,13 @@
 import os
 import collections
-from utils import get_top_verbs_in_path
+from utils import get_top_verbs
+
+
+def print_calc_result(_project, verb_size, words):
+    if words:
+        print('%s: total %s words, %s unique' % (_project, len(words), len(set(words))))
+        for word, occurence in collections.Counter(words).most_common(verb_size):
+            print(word, occurence)
 
 
 def calc_verbs(_project: str, verb_size: int):
@@ -9,11 +16,8 @@ def calc_verbs(_project: str, verb_size: int):
     if not os.path.exists(project_path):
         return
 
-    words = get_top_verbs_in_path(project_path)
-    if words:
-        print('%s: total %s words, %s unique' % (_project, len(words), len(set(words))))
-        for word, occurence in collections.Counter(words).most_common(verb_size):
-            print(word, occurence)
+    words = get_top_verbs(project_path)
+    print_calc_result(_project, verb_size, words)
 
 
 if __name__ == '__main__':
